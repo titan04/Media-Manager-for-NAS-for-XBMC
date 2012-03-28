5f9989e9acd61551cc28ac0c567a299e86a798e7
titan04 <joshconnolly78@gmail.com>
2012-03-28 17:10:24 -0300

    Update application/config/scrapers/video/tvdb_com.php

--- application/config/scrapers/video/tvdb_com.php
+++ application/config/scrapers/video/tvdb_com.php
@@ -1,7 +1,7 @@
 <?php
 
-$config['api_key'] = '1D62F2F90030C444';
-$config['lang'] = '17';
+$config['api_key'] = '7258DDA442492F57';
+$config['lang'] = '7';
 
 /* End of file tvdb_com.php */
 /* Location: ./application/config/scrapers/tvdb_com.php */
