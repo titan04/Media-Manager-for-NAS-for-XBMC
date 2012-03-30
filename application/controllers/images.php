4756bdf98391f13b1b8e1ff41dab02ecd372ff5a
Josh Connolly <joshconnolly78@gmail.com>
2012-03-29 23:22:24 -0400

    Formating and Commenting.

--- application/controllers/images.php
+++ application/controllers/images.php
@@ -1,39 +1,37 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
-class Images extends CI_Controller
-{
-  public function __construct()
-  {
-    parent::__construct();
+class Images extends CI_Controller {
+	public function __construct() {
+		parent::__construct();
 
-    // Chargement des modèles de la base de données 'xbmc_video'
-    $this->load->model('video/actors_model');
-    $this->load->model('video/countries_model');
-    $this->load->model('video/episodes_model');
-    $this->load->model('video/video_files_model');
-    $this->load->model('video/genres_model');
-    $this->load->model('video/movies_model');
-    $this->load->model('video/video_paths_model');
-    $this->load->model('xbmc/sources_model');
-    $this->load->model('video/sets_model');
-    $this->load->model('video/studios_model');
-    $this->load->model('video/tvshows_model');
-  }
+		// Chargement des modèles de la base de données 'xbmc_video'
+		$this -> load -> model('video/actors_model');
+		$this -> load -> model('video/countries_model');
+		$this -> load -> model('video/episodes_model');
+		$this -> load -> model('video/video_files_model');
+		$this -> load -> model('video/genres_model');
+		$this -> load -> model('video/movies_model');
+		$this -> load -> model('video/video_paths_model');
+		$this -> load -> model('xbmc/sources_model');
+		$this -> load -> model('video/sets_model');
+		$this -> load -> model('video/studios_model');
+		$this -> load -> model('video/tvshows_model');
+	}
 
-  /**
-   * Change l'image d'un film, une série TV, une saga ou une personne
-   * après sélection de cette image
-   * Gère les affiches, bannières, photos et fond d'écran
-   */
-	public function ajax_change_image()
-	{
+	/**
+	 * Change l'image d'un film, une série TV, une saga ou une personne
+	 * après sélection de cette image
+	 * Gère les affiches, bannières, photos et fond d'écran
+	 */
+	public function ajax_change_image() {
 
-    // Appel via ajax ?
-    if (IS_AJAX)
-    {
+		// Appel via ajax ?
+		if (IS_AJAX) {
 
 			// Contrôleur_idenfiant dans le champs 'media_id'
-			list($media, $id) = explode('_', $this->input->post('media_id'));
+			list($media, $id) = explode('_', $this -> input -> post('media_id'));
 
 			// Type d'image (poster, banner, photo, backdrop)
 			$type = $_POST['type'];
@@ -44,69 +42,59 @@ class Images extends CI_Controller
 			// Nom et chemin de l'image à changer sur le serveur
 			// Les variables 'poster_filename', 'banner_filename', 'backdrop_filename' sont définies
 			// en javascript selon le type de 'media' consulté
-			$image_filename = $_POST[$type.'_filename'];
+			$image_filename = $_POST[$type . '_filename'];
 
-//echo '<pre>'.print_r($_POST, true).'</pre>'; die();
+			//echo '<pre>'.print_r($_POST, true).'</pre>'; die();
 
 			// Chemin d'une éventuelle image générée automatiquement par xbmc pour la supprimer
-			$file_filename = substr($image_filename, strrpos($image_filename, "/")+1);
-			$file_auto = str_replace($file_filename, 'auto-'.$file_filename, $image_filename);
-			if (file_exists($file_auto)) unlink($file_auto);
+			$file_filename = substr($image_filename, strrpos($image_filename, "/") + 1);
+			$file_auto = str_replace($file_filename, 'auto-' . $file_filename, $image_filename);
+			if (file_exists($file_auto))
+				unlink($file_auto);
 
 			// Téléchargement de l'image et sauvegarde au bon endroit
-			$this->xbmc_lib->download($image_url, $image_filename);
+			$this -> xbmc_lib -> download($image_url, $image_filename);
 			sleep(1);
 
-			switch($type)
-			{
-				case 'poster';
-					$thumbnail_filename = $this->xbmc_lib->images_cache_dir.'xbmc/Video/media/p_'.str_replace('.tbn', '.jpg', substr($image_filename, strrpos($image_filename, '/')+1));
+			switch($type) {
+				case 'poster' :
+					$thumbnail_filename = $this -> xbmc_lib -> images_cache_dir . 'xbmc/Video/media/p_' . str_replace('.tbn', '.jpg', substr($image_filename, strrpos($image_filename, '/') + 1));
 
 					// Mise à jour de la miniature
-					$this->xbmc_lib->create_image($image_filename, $thumbnail_filename, $this->xbmc_lib->poster_size, TRUE);
+					$this -> xbmc_lib -> create_image($image_filename, $thumbnail_filename, $this -> xbmc_lib -> poster_size, TRUE);
 					break;
 
-				case 'banner';
-					$thumbnail_filename = $this->xbmc_lib->images_cache_dir.'xbmc/Video/media/b_'.str_replace('.tbn', '.jpg', substr($image_filename, strrpos($image_filename, '/')+1));
+				case 'banner' :
+					$thumbnail_filename = $this -> xbmc_lib -> images_cache_dir . 'xbmc/Video/media/b_' . str_replace('.tbn', '.jpg', substr($image_filename, strrpos($image_filename, '/') + 1));
 
 					// Mise à jour de la miniature
-					$this->xbmc_lib->create_image($image_filename, $thumbnail_filename, $this->xbmc_lib->banner_size, TRUE);
+					$this -> xbmc_lib -> create_image($image_filename, $thumbnail_filename, $this -> xbmc_lib -> banner_size, TRUE);
 					break;
 
-				case 'backdrop';
-					$thumbnail_filename = $this->xbmc_lib->images_cache_dir.'xbmc/Video/Fanart/b_'.str_replace('.tbn', '.jpg', substr($image_filename, strrpos($image_filename, '/')+1));
+				case 'backdrop' :
+					$thumbnail_filename = $this -> xbmc_lib -> images_cache_dir . 'xbmc/Video/Fanart/b_' . str_replace('.tbn', '.jpg', substr($image_filename, strrpos($image_filename, '/') + 1));
 
 					// Mise à jour de la miniature
-					$this->xbmc_lib->create_image($image_filename, $thumbnail_filename, $this->xbmc_lib->backdrop_size, TRUE);
+					$this -> xbmc_lib -> create_image($image_filename, $thumbnail_filename, $this -> xbmc_lib -> backdrop_size, TRUE);
 					break;
 			}
 
 			// Url de l'image miniature sur le site
-			$url = str_replace($this->xbmc_lib->images_cache_dir, $this->xbmc_lib->images_cache_url, $thumbnail_filename).'?'.time();
+			$url = str_replace($this -> xbmc_lib -> images_cache_dir, $this -> xbmc_lib -> images_cache_url, $thumbnail_filename) . '?' . time();
 
-//echo $url; die();
+			//echo $url; die();
 
-			switch($media)
-			{
-				case 'movies':
-					$json = array('success' => '1',
-												'url' => $url,
-												'message' => $this->lang->line('msg_'.$type.'_movie_updated')
-											 );
+			switch($media) {
+				case 'movies' :
+					$json = array('success' => '1', 'url' => $url, 'message' => $this -> lang -> line('msg_' . $type . '_movie_updated'));
 					break;
 
-				case 'sets':
-					$json = array('success' => '1',
-												'url' => $url,
-												'message' => $this->lang->line('msg_'.$type.'_set_updated')
-											 );
+				case 'sets' :
+					$json = array('success' => '1', 'url' => $url, 'message' => $this -> lang -> line('msg_' . $type . '_set_updated'));
 					break;
 
-				case 'tvshows':
-					$json = array('success' => '1',
-												'url' => $url,
-												'message' => $this->lang->line('msg_'.$type.'_tvshow_updated')
-											 );
+				case 'tvshows' :
+					$json = array('success' => '1', 'url' => $url, 'message' => $this -> lang -> line('msg_' . $type . '_tvshow_updated'));
 					break;
 			}
 
@@ -118,34 +106,34 @@ class Images extends CI_Controller
 		}
 	}
 
-  /**
-   * Ajoute une nouvelle image à un film, une série TV, une saga ou une personne
-   * Gère les affiches, bannières, photos et fond d'écran
-   */
-	public function ajax_upload_image()
-	{
-		$type = $this->input->post('type');
-		$image_filename = $this->input->post('image_filename');
+	/**
+	 * Ajoute une nouvelle image à un film, une série TV, une saga ou une personne
+	 * Gère les affiches, bannières, photos et fond d'écran
+	 */
+	public function ajax_upload_image() {
+		$type = $this -> input -> post('type');
+		$image_filename = $this -> input -> post('image_filename');
 
 		// Chemin d'une éventuelle image générée automatiquement pour suppression
-		$file_filename = substr($image_filename, strrpos($image_filename, "/")+1);
-		$file_auto = str_replace($file_filename, 'auto-'.$file_filename, $image_filename);
-		if (file_exists($file_auto)) unlink($file_auto);
+		$file_filename = substr($image_filename, strrpos($image_filename, "/") + 1);
+		$file_auto = str_replace($file_filename, 'auto-' . $file_filename, $image_filename);
+		if (file_exists($file_auto))
+			unlink($file_auto);
 
 		// Traitement de l'iamge téléchargée
 		// Prévoir gestion des erreurs
-		 $this->_upload($image_filename);
+		$this -> _upload($image_filename);
 
 		// Url de l'image pour mise à jour
-		$url = str_replace($this->config->item('xbmc_thumbnails_dir'), base_url().'assets/images/', $image_filename);
+		$url = str_replace($this -> config -> item('xbmc_thumbnails_dir'), base_url() . 'assets/images/', $image_filename);
 		$url = str_replace('\\', '/', $url);
 
-    echo '<script type="text/javascript">
+		echo '<script type="text/javascript">
     <!--
 			var data = {
 				success: "1",
-        type: "'.$type.'",
-        url: "'.$url.'",
+        type: "' . $type . '",
+        url: "' . $url . '",
         message: "Toto fait du vélo."
 			};
 
@@ -154,16 +142,17 @@ class Images extends CI_Controller
     </script>';
 	}
 
-	private function _upload($target)
-	{
+	private function _upload($target) {
 		$extensions = array('png', 'jpg', 'jpeg');
 
 		// Fichier correctement uploadé
-		if (!isset($_FILES['image']) OR $_FILES['image']['error'] > 0) return FALSE;
+		if (!isset($_FILES['image']) OR $_FILES['image']['error'] > 0)
+			return FALSE;
 
 		// Extension
-		$ext = substr(strrchr($_FILES['image']['name'],'.'),1);
-		if ($extensions !== FALSE AND !in_array($ext, $extensions)) return FALSE;
+		$ext = substr(strrchr($_FILES['image']['name'], '.'), 1);
+		if ($extensions !== FALSE AND !in_array($ext, $extensions))
+			return FALSE;
 
 		// Déplacement
 		return move_uploaded_file($_FILES['image']['tmp_name'], $target);
